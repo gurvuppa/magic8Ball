e633783de0cb35e7733f8c844934d15450e31a23
Gurvir Uppal <gurviruppal@icloud.com>
2021-05-06 14:22:35 -0400

    Add project files.

---
+++ magic8Ball/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pressButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pressButton
+            // 
+            this.pressButton.BackColor = System.Drawing.Color.Transparent;
+            this.pressButton.FlatAppearance.BorderSize = 0;
+            this.pressButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.pressButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.pressButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pressButton.Location = new System.Drawing.Point(248, 192);
+            this.pressButton.Name = "pressButton";
+            this.pressButton.Size = new System.Drawing.Size(150, 134);
+            this.pressButton.TabIndex = 1;
+            this.pressButton.UseVisualStyleBackColor = false;
+            this.pressButton.Click += new System.EventHandler(this.pressButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputLabel.Location = new System.Drawing.Point(151, 491);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(340, 56);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(223, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Think of a question";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(635, 572);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.pressButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pressButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ magic8Ball/Form1.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Gurvir Uppal
+ * Magic 8 Ball
+ * May 6 2021
+ * Mr. T 
+ * ICS 3U
+ * */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace magic8Ball
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+
+        int number = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pressButton_Click(object sender, EventArgs e)
+        {
+
+            number = randGen.Next(1, 7);
+            switch (number)
+            {
+                case 1:
+                    outputLabel.Text = "Most definatly"; 
+                    break;
+                case 2:
+                    outputLabel.Text = "Probably";
+                    break;
+                case 3:
+                    outputLabel.Text = "Maybe";
+                    break;
+                case 4:
+                    outputLabel.Text = "Probably not";
+                    break;
+                case 5:
+                    outputLabel.Text = "Not happening";
+                    break;
+                case 6:
+                    outputLabel.Text = "Not in a million  years buddy";
+                    break;
+            }
+        }
+    }
+}
